30f694a0a428a446cc50d38d9e050bd763d6dd39
unknown <ofir.yaniv01@post.runi.ac.il>
2025-11-18 20:23:26 +0200

    another try

--- MyString.java
+++ MyString.java
@@ -24,8 +24,11 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        if (str1.length() == 0 || str2.length() == 0) {
+        if (str1.length() == 0) {
             return false;
+        }
+        if (str2.length() == 0) {
+            return true;
         }
         int index = 0;
         for(int i = 0; i < str1.length(); i++){
